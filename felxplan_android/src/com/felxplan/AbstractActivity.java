a8b57e0a07946baa28f5abc2a07d55002b3d96e1
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-01-09 12:00:34 +0100

    fullscreenactivity functionality outsourced in abstract class

---
+++ felxplan_android/src/com/felxplan/AbstractActivity.java
@@ -0,0 +1,107 @@
+package com.felxplan;
+
+import com.felxplan.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.view.MotionEvent;
+import android.view.View;
+
+public abstract class AbstractActivity extends FragmentActivity{
+	
+	private static final boolean AUTO_HIDE = true;
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+	private static final boolean TOGGLE_ON_CLICK = true;
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+	private SystemUiHider mSystemUiHider;
+	
+	protected View controlsView;
+
+	protected View contentView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
+				HIDER_FLAGS);
+		mSystemUiHider.setup();
+		mSystemUiHider
+				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+					int mControlsHeight;
+					int mShortAnimTime;
+
+					@Override
+					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+					public void onVisibilityChange(boolean visible) {
+						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+							if (mControlsHeight == 0) {
+								mControlsHeight = controlsView.getHeight();
+							}
+							if (mShortAnimTime == 0) {
+								mShortAnimTime = getResources().getInteger(
+										android.R.integer.config_shortAnimTime);
+							}
+							controlsView
+									.animate()
+									.translationY(visible ? 0 : mControlsHeight)
+									.setDuration(mShortAnimTime);
+						} else {
+							controlsView.setVisibility(visible ? View.VISIBLE
+									: View.GONE);
+						}
+
+						if (visible && AUTO_HIDE) {
+							delayedHide(AUTO_HIDE_DELAY_MILLIS);
+						}
+					}
+				});
+
+		contentView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (TOGGLE_ON_CLICK) {
+					mSystemUiHider.toggle();
+				} else {
+					mSystemUiHider.show();
+				}
+			}
+		});
+		findViewById(R.id.add_break_button).setOnTouchListener(
+				mDelayHideTouchListener);
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		delayedHide(100);
+	}
+	
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (AUTO_HIDE) {
+				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+			}
+			return false;
+		}
+	};
+	
+	Handler mHideHandler = new Handler();
+	Runnable mHideRunnable = new Runnable() {
+		@Override
+		public void run() {
+			mSystemUiHider.hide();
+		}
+	};
+	
+	private void delayedHide(int delayMillis) {
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+	}
+	
+
+}
--- felxplan_android/src/com/felxplan/BreakViewActivity.java
+++ felxplan_android/src/com/felxplan/BreakViewActivity.java
@@ -1,12 +1,6 @@
 package com.felxplan;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ListView;
 
 import com.felxplan.util.SystemUiHider;
@@ -18,14 +12,9 @@ import com.flexplan.common.business.FlextimeDay;
  * 
  * @see SystemUiHider
  */
-public class BreakViewActivity extends Activity {
-	private static final boolean AUTO_HIDE = true;
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-	private static final boolean TOGGLE_ON_CLICK = true;
-	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-	private SystemUiHider mSystemUiHider;
+public class BreakViewActivity extends AbstractActivity {
+
 	private FlextimeDay currentFlextimeDay;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,87 +22,13 @@ public class BreakViewActivity extends Activity {
 
 		setContentView(R.layout.activity_break_view);
 
-		final View controlsView = findViewById(R.id.fullscreen_content_controls);
-		final View contentView = findViewById(R.id.fullscreen_content);
+		controlsView = findViewById(R.id.fullscreen_content_controls);
+		contentView = findViewById(R.id.fullscreen_content);
 
 		ListView breakListView = (ListView) findViewById(R.id.breakList);
-		breakListView.setAdapter(new BreakListAdapter(getApplicationContext(),currentFlextimeDay.getWorkBreaks()));
-		
-		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
-				HIDER_FLAGS);
-		mSystemUiHider.setup();
-		mSystemUiHider
-				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-					int mControlsHeight;
-					int mShortAnimTime;
+		breakListView.setAdapter(new BreakListAdapter(getApplicationContext(),
+				currentFlextimeDay.getWorkBreaks()));
 
-					@Override
-					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-					public void onVisibilityChange(boolean visible) {
-						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-							if (mControlsHeight == 0) {
-								mControlsHeight = controlsView.getHeight();
-							}
-							if (mShortAnimTime == 0) {
-								mShortAnimTime = getResources().getInteger(
-										android.R.integer.config_shortAnimTime);
-							}
-							controlsView
-									.animate()
-									.translationY(visible ? 0 : mControlsHeight)
-									.setDuration(mShortAnimTime);
-						} else {
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
-						}
-
-						if (visible && AUTO_HIDE) {
-							delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						}
-					}
-				});
-
-		contentView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (TOGGLE_ON_CLICK) {
-					mSystemUiHider.toggle();
-				} else {
-					mSystemUiHider.show();
-				}
-			}
-		});
-		findViewById(R.id.add_break_button).setOnTouchListener(
-				mDelayHideTouchListener);
 	}
 
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		delayedHide(100);
-	}
-
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-			}
-			return false;
-		}
-	};
-
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mSystemUiHider.hide();
-		}
-	};
-
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
-	}
 }
