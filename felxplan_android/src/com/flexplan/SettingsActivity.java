0dc7d251e51b0fd2521d26e2d2ad5ea2554e6302
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-03-04 08:14:02 +0100

    cleanup strings for settings

--- felxplan_android/src/com/flexplan/SettingsActivity.java
+++ felxplan_android/src/com/flexplan/SettingsActivity.java
@@ -2,7 +2,6 @@ package com.flexplan;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -15,18 +14,6 @@ public class SettingsActivity extends PreferenceActivity {
 
 	@SuppressWarnings("deprecation")
 	private void setupSimplePreferencesScreen() {
-
 		addPreferencesFromResource(R.xml.flexplan_preference);
-
-		// Add 'notifications' preferences, and a corresponding header.
-		PreferenceCategory fakeHeader = new PreferenceCategory(this);
-		fakeHeader.setTitle(R.string.pref_header_notifications);
-		getPreferenceScreen().addPreference(fakeHeader);
-
-		// Add 'data and sync' preferences, and a corresponding header.
-		fakeHeader = new PreferenceCategory(this);
-		fakeHeader.setTitle(R.string.pref_header_data_sync);
-		getPreferenceScreen().addPreference(fakeHeader);
-
 	}
 }
